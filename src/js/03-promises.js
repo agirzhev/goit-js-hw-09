998a048047de0d3fd1ba4430f36aeccc7d66c21e
agirzhev <a.girgev@gmail.com>
2022-05-23 17:20:06 +0200

    Finish 2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,20 +4,27 @@ const form = document.querySelector('.form');
 const fistDeplay = document.querySelectorAll('input')[0]
 const stepDeplay = document.querySelectorAll('input')[1]
 const amountDeplay = document.querySelectorAll('input')[2]
+let time = 0
 
-function createPromise(position, delay) {
+function createPromise(position, delay, step) {
+  if (step === 0) {
+    time = 0
+    time = time + Number(position)
+  } else {
+    time = time + Number(delay)
+  }
+
+  
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
       
       if (shouldResolve) {
         // Fulfill
-        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`)
+        resolve(Notiflix.Notify.success(`✅ Fulfilled promise ${step+1} in ${time}ms`)) 
       } else {
-        reject(`❌ Rejected promise ${position} in ${delay}ms`)
+        reject(Notiflix.Notify.failure(`❌ Rejected promise ${step+1} in ${time}ms`))  
       }
-    },delay)
-
+    
   })
 }
 
@@ -25,13 +32,16 @@ form.addEventListener("submit", function (event) {
 
   event.preventDefault()
 
-  for (let i = 0; i <= amountDeplay.value; i += 1) {
-    createPromise(fistDeplay.value, stepDeplay.value)
-      .then(value => {Notiflix.Notify.success(value)
-      })
-      .catch(error => {Notiflix.Notify.failure(error)
-
-      })
+  for (let i = 0; i <= amountDeplay.value - 1; i += 1) {
+    if (i === 0) {
+      setTimeout(
+        createPromise(fistDeplay.value, stepDeplay.value, i), fistDeplay.value
+      )  
+    } else {
+      setTimeout(
+        createPromise(fistDeplay.value, stepDeplay.value, i), stepDeplay.value
+      )
+    }
 }   
 
 } 
